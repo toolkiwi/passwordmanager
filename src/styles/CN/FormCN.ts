69a6797cec6182ab4b84fae6d1dd506aad05d503
shiftytab <rodrigoestevesramos@gmail.com>
2025-06-03 22:35:43 +0200

    styled(passwords): make row size in virtualizer responsive with useIsMobile hook

--- src/styles/CN/FormCN.ts
+++ src/styles/CN/FormCN.ts
@@ -6,9 +6,9 @@ export default {
     input_group: 'flex flex-col',
     container: 'p-5 flex flex-col gap-5',
     favicon_container:
-        'relative w-12 h-12 flex items-center justify-center bg-black/80 p-2 border-1 border-neutral-900 rounded-lg',
+        'relative w-12 h-12 flex items-center justify-center bg-black/80 p-2 border-1 border-neutral-900 rounded-lg max-sm:w-10 max-sm:h-10',
     favicon: 'w-full h-full rounded-md',
     label: 'text-white text-sm font-bold uppercase',
-    sub_label: 'text-neutral-600 mt-1 mb-2 text-md',
+    sub_label: 'text-neutral-600 mt-1 mb-2 text-md max-sm:text-sm truncate',
     footer: 'p-5 flex flex-row items-center gap-5',
 };
--- src/styles/CN/ListCN.ts
+++ src/styles/CN/ListCN.ts
@@ -3,17 +3,17 @@
  */
 export default {
     container: 'relative flex-1',
-    wrapper: 'p-5 overflow-auto flex-1 h-full max-h-[calc(100vh-175px)]',
+    wrapper: 'p-5 overflow-auto flex-1 h-full',
     rows_virtualizer: 'relative flex flex-col gap-5',
     list: 'mt-5 flex flex-col gap-4 p-5',
     list_item:
-        'h-[100px] cursor-pointer p-5 border border-neutral-900 hover:bg-neutral-900/50 rounded-xl flex flex-row items-center group/li active:bg-neutral-900',
-    list_item_content: 'flex-1 flex flex-row items-center',
-    text_container: 'flex-1 px-5',
+        'h-[100px] cursor-pointer p-5 border border-neutral-900 hover:bg-neutral-900/50 rounded-xl flex flex-row items-center group/li active:bg-neutral-900 max-sm:h-[85px]',
+    list_item_content: 'flex-1 flex flex-row items-center overflow-hidden',
+    text_container: 'flex-1 px-5 max-sm:px-4 truncate',
     favicon_container:
-        'w-12 h-12 flex items-center justify-center bg-black/80 p-2 border-1 border-neutral-900 rounded-lg',
+        'w-12 h-12 flex items-center justify-center bg-black/80 p-2 border-1 border-neutral-900 rounded-lg min-w-10 min-h-10',
     favicon: 'w-full h-full rounded-md',
-    title: 'text-neutral-200 font-semibold text-md',
+    title: 'text-neutral-200 font-semibold text-md max-sm:text-sm truncate',
     actions: 'flex flex-row items-center gap-2.5 z-10',
-    sub_title: 'text-neutral-500 text-sm',
+    sub_title: 'text-neutral-500 text-sm max-sm:text-xs! truncate!',
 };
