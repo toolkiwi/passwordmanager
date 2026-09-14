0978682b6c5b64d6c04dd38053a1d5e454e24a83
shiftytab <rodrigoestevesramos@gmail.com>
2026-09-14 12:06:23 +0200

    refactor: simplify state management in StyledCheckbox component

--- src/components/styled/form/StyledCheckbox.tsx
+++ src/components/styled/form/StyledCheckbox.tsx
@@ -14,34 +14,12 @@ export default function StyledCheckbox({
     value?: boolean;
     wrapperClassName?: React.HTMLAttributes<HTMLDivElement>['className'];
 }): React.ReactElement {
-    const [checked, setChecked] = React.useState(value ?? false);
-    const [initialized, setInitialized] = React.useState(false);
-
-    /**
-     * Callback when checked state changes
-     * Only call onChange if component is initialized to avoid calling onChange on mount
-     */
-    React.useEffect(() => {
-        if (initialized) {
-            onChange(checked);
-        } else {
-            setInitialized(true);
-        }
-    }, [checked, initialized, onChange]);
-
-    /**
-     * Update checked state when value prop changes
-     */
-    React.useEffect(() => {
-        if (value !== undefined) {
-            setChecked(value);
-        }
-    }, [value]);
+    const checked = value ?? false;
 
     return (
         <div
             className={`flex flex-row items-center gap-3 border hover:bg-foreground/2 rounded-lg p-2 cursor-pointer select-none ${wrapperClassName ? wrapperClassName : ''}`}
-            onClick={() => setChecked(!checked)}
+            onClick={() => onChange(!checked)}
         >
             <div className='w-5 h-5 rounded-md border border-foreground/10! flex items-center justify-center cursor-pointer'>
                 {checked && <TbCheck className='text-foreground w-3 h-3' />}
